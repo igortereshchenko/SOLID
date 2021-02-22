7c56c84766bc0a1d00a9fdd612119318f71dd545
igortereshchenko <tereshchenko.igor@gmail.com>
2021-02-22 11:42:05 +0200

    Task 1 (SO)

---
+++ Task 1.py
@@ -0,0 +1,24 @@
+class Human:
+    def __init__(self, inn, name, driving_licence, address, phome, car, birthday):
+        self.inn = inn
+        # ....
+        self.bithday = birthday
+
+    def add_new_carr(self, car):
+        pass
+    def add_new_address(self, address):
+        pass
+    def add_new_driving_licence(self, driving_licence):
+        pass
+
+    def search_human_by_car(self, car):
+        pass
+    def search_human_by_name_and_address(self, name, address):
+        pass
+    def search_human_by_car_and_address(self, car, address):
+        pass
+
+
+    def load_human(self):
+        # load from db
+        pass
